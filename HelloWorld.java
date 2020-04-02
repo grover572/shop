f3be45ed84569495d984b77a93bf14618e7211d7
grover572 <996493520@qq.com>
2020-04-02 22:37:07 +0800

    第一个GitHub程序

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @Package:PACKAGE_NAME
+ * @Author:Grover 2020/4/2 21:56:25
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git And World!");
+    }
+}
